a1a48b500c3dc00b163cdd5178fefd200c8f5e68
mromig09 <mromig09@users.noreply.github.com>
2019-08-05 12:23:45 +1000

    rainbow

---
+++ week 02/chalk/src/index.ts
@@ -0,0 +1,6 @@
+import Chalk from"chalk";
+//const chalk = require('chalk');
+ 
+console.log(Chalk.blue('H'), Chalk.red('E'), Chalk.magenta('L'),
+Chalk.yellow('L'), Chalk.cyan('O'), (''), Chalk.green('W'),
+Chalk.blue('O'), Chalk.red('R'), Chalk.magenta('L'), Chalk.yellow('D'));
