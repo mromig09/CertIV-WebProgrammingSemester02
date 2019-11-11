043c757f1bf147bc66204ad1afc13c226c0a1b19
mromig09 <mromig09@users.noreply.github.com>
2019-11-11 14:11:24 +1100

    start

--- planets-angular/planet-app/src/app/app.module.ts
+++ planets-angular/planet-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlanetDetailComponent } from './planet-detail/planet-detail.component';
+import { PlanetCreateComponent } from './planet-create/planet-create.component';
+import { PlanetListComponent } from './planet-list/planet-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetDetailComponent,
+    PlanetCreateComponent,
+    PlanetListComponent
   ],
   imports: [
     BrowserModule
---
+++ planets-angular/planet-app/src/app/planet-create/planet-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planet-create',
+  templateUrl: './planet-create.component.html',
+  styleUrls: ['./planet-create.component.css']
+})
+export class PlanetCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
